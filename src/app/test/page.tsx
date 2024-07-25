e5c1652c2f4ae75f99fa15cc1fe397a1242c42f6
shizheng <64840669+tszones@users.noreply.github.com>
2024-07-25 17:19:47 +0800

    update test page

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,5 +1,18 @@
 "use client"
 
+import Link from "next/link"
+
 export default function TestPage (){
-    return <div>Test Page</div>
+    return (
+        <>
+            <div>
+                expand-from-center
+            </div>
+            <Link 
+                href={"https://www.google.com"}
+                className="relative after:content-[''] after:absolute after:left-1/2 after:right-1/2 after:bottom-0    after:h-[2px] after:bg-foreground after:transition-all hover:after:left-0 hover:after:right-0"
+                target="_blank"
+            >Google</Link>
+        </>
+    )
 }
