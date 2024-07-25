d52252e97a9df86722de3d48b61efc86936f92cd
shizheng <64840669+tszones@users.noreply.github.com>
2024-07-25 16:44:27 +0800

    add autosize-textarea

---
+++ src/components/demo/autosize-textarea-customize.tsx
@@ -0,0 +1,98 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import * as React from 'react';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Textarea } from '@/components/ui/textarea';
+import { toast } from '@/components/ui/use-toast';
+import { useAutosizeTextArea } from '@/components/ui-pro/autosize-textarea';
+import { Button } from '@/components/ui/button';
+
+const FormSchema = z.object({
+  bio: z
+    .string()
+    .min(10, {
+      message: 'Bio must be at least 10 characters.',
+    })
+    .max(160, {
+      message: 'Bio must not be longer than 30 characters.',
+    }),
+});
+
+const AutosizeTextareaCustomize = () => {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  const [loading, setLoading] = React.useState(false);
+
+  const textAreaRef = React.useRef<HTMLTextAreaElement>(null);
+  const [triggerAutoSize, setTriggerAutoSize] = React.useState('');
+  useAutosizeTextArea({
+    textAreaRef: textAreaRef?.current,
+    triggerAutoSize: triggerAutoSize,
+    minHeight: 52,
+    maxHeight: 200,
+  });
+
+  /** You can use `form.watch` to trigger auto sizing. */
+  const bio = form.watch('bio');
+  React.useEffect(() => {
+    if (textAreaRef.current) {
+      setTriggerAutoSize(bio);
+    }
+  }, [bio]);
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+      toast({
+        title: 'Your submitted data',
+        description: (
+          <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+            <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+          </pre>
+        ),
+      });
+    }, 500);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+        <FormField
+          control={form.control}
+          name="bio"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Bio</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Tell us a little bit about yourself"
+                  {...field}
+                  ref={textAreaRef}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
+};
+export default AutosizeTextareaCustomize;
---
+++ src/components/demo/autosize-textarea-form.tsx
@@ -0,0 +1,95 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import * as React from 'react';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { toast } from '@/components/ui/use-toast';
+import { AutosizeTextarea } from '@/components/ui-pro/autosize-textarea';
+import { Button } from '@/components/ui/button';
+import { useState } from 'react';
+
+const FormSchema = z.object({
+  bio: z.string().min(10, {
+    message: 'Bio must be at least 10 characters.',
+  }),
+});
+
+const DEFAULT_VALUE = {
+  bio: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+};
+
+const AutosizeTextareaForm = () => {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    defaultValues: DEFAULT_VALUE,
+    resolver: zodResolver(FormSchema),
+  });
+
+  const [isClearBio, setIsClearBio] = useState(false);
+
+  const [loading, setLoading] = React.useState(false);
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+      toast({
+        title: 'Your submitted data',
+        description: (
+          <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+            <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+          </pre>
+        ),
+      });
+      if (isClearBio) {
+        form.setValue('bio', '');
+      }
+    }, 500);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+        <span className="flex gap-2">
+          <input
+            type="checkbox"
+            id="should-clear-bio"
+            className="cursor-pointer"
+            checked={isClearBio}
+            onChange={() => setIsClearBio((pre) => !pre)}
+          />
+          <label htmlFor="should-clear-bio" className="cursor-pointer">
+            Clear bio after submit.
+          </label>
+        </span>
+
+        <FormField
+          control={form.control}
+          name="bio"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel htmlFor="bio">Bio</FormLabel>
+              <FormControl>
+                <AutosizeTextarea id="bio" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
+};
+export default AutosizeTextareaForm;
