c532b8cd72939e796673c44f9e1af060b4111b14
shizheng <64840669+tszones@users.noreply.github.com>
2024-07-25 17:30:39 +0800

    remove google fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import "./globals.css";
 import { RootProvider } from 'fumadocs-ui/provider';
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-        <body className={inter.className}>
+        <body>
         <RootProvider>
           {children}
         </RootProvider>
