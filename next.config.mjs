d2179f2c766f50ccf78204816d81c23ec482b016
shizheng <64840669+tszones@users.noreply.github.com>
2024-07-31 15:12:13 +0800

    add remarkInstall

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,19 @@
 import createMDX from 'fumadocs-mdx/config';
+import { remarkInstall } from 'fumadocs-docgen';
  
 const withMDX = createMDX({
     include: ['./**/*.{md,mdx,json}'],
+    mdxOptions: {
+      lastModifiedTime: 'git',
+      remarkPlugins: [
+        [remarkInstall],
+      ],
+    },
   });
  
 /** @type {import('next').NextConfig} */
 const config = {
-    reactStrictMode: true,
+  reactStrictMode: true,
 };
  
 export default withMDX(config);
