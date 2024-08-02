e2f94c639762251a0dd88602d90b64e46b5acf0f
Arthur Sept <codeCola.js@outlook.com>
2024-08-03 00:10:11 +0800

    add component loading-button

---
+++ src/components/demo/loading-button-demo.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import React from 'react';
+import { LoadingButton } from '@/components/ui-pro/loading-button';
+
+export const LoadingButtonDemo = () => {
+  return <LoadingButton loading>loading button</LoadingButton>;
+};
+
