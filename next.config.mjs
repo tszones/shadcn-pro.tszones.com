bcb18c9d3377a4226352e6e691ccd68fd32c7237
shizheng <64840669+tszones@users.noreply.github.com>
2024-08-02 09:43:07 +0800

    add plugin fileGenereator

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,15 @@
 import createMDX from 'fumadocs-mdx/config';
 import { remarkInstall } from 'fumadocs-docgen';
+import { remarkDocGen, fileGenerator } from 'fumadocs-docgen';
  
+
 const withMDX = createMDX({
     include: ['./**/*.{md,mdx,json}'],
     mdxOptions: {
       lastModifiedTime: 'git',
       remarkPlugins: [
         [remarkInstall],
+        [remarkDocGen, { generators: [fileGenerator()] }]
       ],
     },
   });
