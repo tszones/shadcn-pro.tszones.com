d44121a770f6c44bb385fc0dcf5580324bf5ae66
shizheng <64840669+tszones@users.noreply.github.com>
2024-08-02 10:16:46 +0800

    add  ga code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 // import { Inter } from "next/font/google";
 import "./globals.css";
 import { RootProvider } from 'fumadocs-ui/provider';
+import GoogleAnalytics from "@/components/google-analytics";
 
 // const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
         <body>
+        <GoogleAnalytics />
         <RootProvider>
           {children}
         </RootProvider>
